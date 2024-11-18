badac71af19e4ca245a7e8555c5f325b8108056d
Yogiswara <101333887+yogiswara92@users.noreply.github.com>
2024-11-18 09:39:30 +0700

    Update QuestionInput.tsx

--- src/components/QuestionInput.tsx
+++ src/components/QuestionInput.tsx
@@ -20,19 +20,25 @@ const QuestionInput: React.FC<QuestionInputProps> = ({ onSubmitQuestions }) => {
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h2>Input Numbers</h2>
-      <input
-        type="number"
-        value={currentNumber}
-        onChange={(e) => setCurrentNumber(e.target.value)}
-        placeholder="Enter a number"
-      />
-      <button onClick={handleAddNumber}>Add Number</button>
-      <button onClick={handleFinishInput}>Finish</button>
-      <div>
-        <h4>Numbers Added:</h4>
-        <p>{questions.join(', ')}</p>
+    <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
+      <h1 style={{ textAlign: 'center', color: '#000000' }}>
+        Save The Mouse!{' '}
+        <div style={{ fontSize: '12px' }}>by Yogiswara Gheartha</div>
+      </h1>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <h2>Input Numbers</h2>
+        <input
+          type="number"
+          value={currentNumber}
+          onChange={(e) => setCurrentNumber(e.target.value)}
+          placeholder="Enter a number"
+        />
+        <button onClick={handleAddNumber}>Add Number</button>
+        <button onClick={handleFinishInput}>Finish</button>
+        <div>
+          <h4>Numbers Added:</h4>
+          <p>{questions.join(', ')}</p>
+        </div>
       </div>
     </div>
   );
